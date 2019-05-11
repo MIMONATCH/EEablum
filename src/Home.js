02bc261b523b9a1ad67e0b98132d0d22f14f4220
yuyy <326018984@qq.com>
2019-05-11 18:38:34 +0800

    yuyy201905111838

--- src/Home.js
+++ src/Home.js
@@ -5,19 +5,28 @@ class Home extends Component{
     constructor(props){
         super(props);
         this.state = {
-            one: 'hello, hahahahah'
+            one: 'hello, hahahahah',
+            two:'nono!',
+            isFlag:true,
         }
     }
 
     render(){
         return (
-            <Fragment>
-                <div id="menu">
-                </div>
-            </Fragment>
+            <div>
+                <Fragment>
+                    <div id="menu">
+                    </div>
+                </Fragment>
+                <h1>{this.state.isFlag ? this.state.one:this.state.two}</h1>
+                <button onClick={()=>{
+                    this.setState({isFlag:!this.state.isFlag});
+                }}>button
+                </button>
+            </div>
+
         );
     }
 
 }
-
 export default Home;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import Home from './Home'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
-//ReactDOM.render(</>, document.getElementById(''))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
